f134bdded801562a337d44f5c359e72beaff53b8
JacopoVisentin <115699372+JacopoVisentin@users.noreply.github.com>
2022-11-20 20:28:22 +0100

    Add files via upload

---
+++ Mademoiselles.py
@@ -0,0 +1,13 @@
+print("Salut! Bienvenu à rue de l'Aerschott!")
+
+name = input("Comment t'appelles tu? \n\n")
+
+print("\nBonjour " + name + ", je t'en prie, rentre dans le salon. \n\n\n")
+
+menu = "\n Une bite dans le cul \t Une bite dans la bouche \t Une bite dans la daronne \t Une bite dans le daron"
+
+print("Bon, du coup, que puis-je faire pour toi " + name + "? Voici le menu si tu veux. A toi le choix: \n\n" + menu)
+
+order = input()
+
+print("\n Ca me semble un bon choix " + name + ", je vais chercher mon gode tout de suite pour te mettre " + order) 
